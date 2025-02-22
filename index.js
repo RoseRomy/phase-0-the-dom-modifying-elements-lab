83b2b20a6d893b8561f9f4d7630179844dececa8
+ <RoseKamau@Romys-Macboook-Air.local>
2025-02-22 21:22:06 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Select the <ul> element and remove it from the DOM
+document.querySelector("ul").remove();
+// Create a new <h1> element 
+const newHeader = document.createElement("h1");
+// Set the id of newHeader the "victory"
+newHeader.id = "victory";
+// Set the text content of newHeader
+newHeader.textContent = "Romyrose is the champion";
+// Append the new <h1> to the body
+document.body.appendChild(newHeader);
